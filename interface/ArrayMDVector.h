09bcc9100d449dc24e52fa5ca7b0c1303448f6b9
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2019-10-09 15:47:29 -0500

    Added defaults for backwards compatibility

--- interface/ArrayMDVector.h
+++ interface/ArrayMDVector.h
@@ -20,6 +20,7 @@ class ArrayMDVector : public std::vector<typename ArrayMDVector<T,d-1>::type> {
         typedef typename ArrayMDVector<T,d-1>::type content_type;
         typedef ArrayMDVector<T,d> type;
         typename ArrayMDVector<T,d>::reference operator[](std::size_t pos);
+        content_type default_val() { return default_value((ArrayMDVector<T,d>*)nullptr); }
 };
 
 template<class T>
@@ -28,6 +29,20 @@ class ArrayMDVector<T,0> {
         typedef T vector_type;
         typedef T type;
 };
+
+//When operator[] is called out of bounds, construct defaults up to that point
+//For a vector type, this is just the default constructor for the vector
+//For a floating point type, this is -999 for backwards compatibility
+//For a bool, this is false for backwards compatibility
+
+template <class T,int d>
+inline typename ArrayMDVector<T,d>::content_type default_value(ArrayMDVector<T,d>*) { return typename ArrayMDVector<T,d>::content_type(); }
+
+template <class T>
+inline T default_value(ArrayMDVector<T,1>*) { return T(-999); }
+
+template <>
+inline bool default_value(ArrayMDVector<bool,1>*) { return false; }
 
 template <class T,int d>
 typename ArrayMDVector<T,d>::vector_type* cast_to_vector(ArrayMDVector<T,d>* arrayVector) {
@@ -38,12 +53,12 @@ template <class T, int d>
 typename ArrayMDVector<T,d>::reference ArrayMDVector<T,d>::operator[](std::size_t pos) {
     std::size_t current_length=this->size();
     if(pos==current_length) {
-        this->push_back(typename ArrayMDVector<T,d>::content_type());
+        this->push_back(ArrayMDVector<T,d>::default_val());
     }
     else if(pos>current_length) {
         std::cout << "Warning: assigned past end of ArrayMDVector. Previous length: " << current_length << " Position: " << pos << ". This is probably a bug.\n";
         for(std::size_t i=0;i<=pos-current_length;++i) {
-            this->push_back(typename ArrayMDVector<T,d>::content_type());
+            this->push_back(ArrayMDVector<T,d>::default_val());
         }
     }
     return std::vector<typename ArrayMDVector<T,d>::content_type>::operator[](pos);
