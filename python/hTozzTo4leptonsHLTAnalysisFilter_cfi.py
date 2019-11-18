6b48b28a6b0da36bf9c3078950e18fb6ca76df6e
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2019-11-18 12:02:08 -0600

    Added verbosity to trigger setup

--- python/hTozzTo4leptonsHLTAnalysisFilter_cfi.py
+++ python/hTozzTo4leptonsHLTAnalysisFilter_cfi.py
@@ -9,5 +9,16 @@ hTozzTo4leptonsHLTAnalysisFilter = cms.EDFilter("HZZ4LeptonsHLTAnalysisFilter",
 def set_triggers(year,dataset):
     print "Setting up trigger for year %i on dataset %s" % (year,dataset)
     triggers=get_triggers(year,dataset)
+    print "Triggering on events that pass the triggers:"
+    for trigger in triggers['pass_triggers']:
+        print trigger
+    veto=triggers['veto_triggers']
+    if veto:
+        print "Except those that pass the triggers:"
+        for trigger in veto:
+            print trigger
+    else:
+        print "No veto triggers."
     hTozzTo4leptonsHLTAnalysisFilter.pass_triggers=cms.vstring(triggers['pass_triggers'])
     hTozzTo4leptonsHLTAnalysisFilter.veto_triggers=cms.vstring(triggers['veto_triggers'])
+    print "Trigger configuration done."
