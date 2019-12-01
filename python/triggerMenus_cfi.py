1b5749cacfb1fdaa85e88322417029d070f794ea
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2019-12-01 16:27:51 -0600

    Synchronized trigger menu to https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsZZ4lRunIILegacy and added 2016 menu

--- python/triggerMenus_cfi.py
+++ python/triggerMenus_cfi.py
@@ -22,87 +22,162 @@ class TriggersByDataset:
                 raise
 
 Triggers={}
+#2016 Trigger Menu
+Triggers[2016]=TriggersByDataset()
+Triggers[2016].add_dataset('DoubleEG',
+    [
+    #dielectron
+    "HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v",
+    #trielectron
+    "HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL_v",
+    ]
+)
+
+Triggers[2016].add_dataset('DoubleMuon',
+    [
+    #dimuon
+    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
+    "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v",
+    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v",
+    "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v",
+    #trimuon
+    "HLT_TripleMu_12_10_5_v",
+    ]
+)
+
+Triggers[2016].add_dataset('MuonEG',
+    [
+    #muele
+    "HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v",
+    "HLT_DiMu9_Ele9_CaloIdL_TrackIdL_v",
+    ]
+)
+
+Triggers[2016].add_dataset('SingleElectron',
+    [
+    #single electron
+    "HLT_Ele25_eta2p1_WPTight_Gsf_v",
+    "HLT_Ele27_WPTight_Gsf_v",
+    "HLT_Ele27_eta2p1_WPLoose_Gsf_v",
+    "HLT_Ele32_eta2p1_WPTight_Gsf_v",
+    ]
+)
+
+Triggers[2016].add_dataset('SingleElectron',
+    [
+    #single muon
+    "HLT_IsoMu20_v",
+    "HLT_IsoTkMu20_v",
+    "HLT_IsoMu22_v",
+    "HLT_IsoTkMu22_v",
+    "HLT_IsoMu24_v",
+    "HLT_IsoTkMu24_v",
+    ]
+)
+
 #2017 Trigger Menu
 Triggers[2017]=TriggersByDataset()
 Triggers[2017].add_dataset('DoubleEG',
     [
-     "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_",
-     "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL",
-     "HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL",
+    #dielectron
+     "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v",
+     "HLT_DoubleEle33_CaloIdL_MW_v",
+    #trielectron
+     "HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL_v",
     ]
 )
 
 Triggers[2017].add_dataset('DoubleMuon',
     [
-     "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8",
-     "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8",
-     "HLT_TripleMu_12_10_5",
-     "HLT_TripleMu_10_5_5_D",
+    #dimuon
+    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v",
+    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v",
+    #trimuon
+    "HLT_TripleMu_10_5_5_DZ_v",
+    "HLT_TripleMu_12_10_5_v",
     ],
 )
 
 Triggers[2017].add_dataset('MuonEG',
     [
-     "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL",
-     "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",
-     "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",
-     "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ",
-     "HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ",
-     "HLT_Mu8_DiEle12_CaloIdL_TrackIdL",
-     "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ",
+    #muele
+    "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ_v",
+    "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v",
+    "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ_v",
     ]
 )
 
 Triggers[2017].add_dataset('SingleElectron',
     [
-     'HLT_Ele35_WPTight_Gsf_v',
-     'HLT_Ele38_WPTight_Gsf_v',
-     'HLT_Ele40_WPTight_Gsf_v',
+    #single electron
+    "HLT_Ele35_WPTight_Gsf_v",
+    "HLT_Ele38_WPTight_Gsf_v",
+    "HLT_Ele40_WPTight_Gsf_v",
     ]
 )
 
 Triggers[2017].add_dataset('SingleMuon',
     [
-     'HLT_IsoMu27',
+    #single muon
+    "HLT_IsoMu27_v",
     ]
 )
 
 Triggers[2018]=TriggersByDataset()
 Triggers[2018].add_dataset('DoubleEG',
     [
-     'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v',
-     'HLT_DoubleEle25_CaloIdL_MW_v',
+    #dielectron
+    "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_DoubleEle25_CaloIdL_MW_v",
+    #all trielectron triggers are prescaled
     ]
 )
 
 Triggers[2018].add_dataset('DoubleMuon',
     [
-     'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v',
-     'HLT_TripleMu_10_5_5_DZ_v',
-     'HLT_TripleMu_12_10_5_v',
+    #dimuon
+    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v",
+    #trimuon
+    "HLT_TripleMu_10_5_5_DZ_v",
+    "HLT_TripleMu_12_10_5_v",
     ]
 )
 
 Triggers[2018].add_dataset('MuonEG',
     [
-     "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v",
-     "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
-     "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
-     "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
-     "HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ_v",
-     "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v",
-     "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ_v",
+    #muele
+    "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v",
+    "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+    "HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ_v",
+    "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ_v",
     ]
 )
 
 Triggers[2018].add_dataset('SingleElectron',
     [
-     'HLT_Ele32_WPTight_Gsf_v',
+    #single electron
+    "HLT_Ele32_WPTight_Gsf_v",
     ]
 )
 Triggers[2018].add_dataset('SingleMuon',
     [
-     'HLT_IsoMu24_v',
+    #single muon
+    "HLT_IsoMu24_v",
     ]
 )
 
